b13eaf8d03834ee15aeb0dcc0ea644253a55236e
System Administrator <root@andelarwandas-MacBook-Pro.local>
2022-08-28 22:18:49 +0200

    shopping action and shopping reducer

---
+++ entry-react-developer-test/src/Redux/Actions/ActionCreators/shoppingAction.js
@@ -0,0 +1,39 @@
+import * as type from "./../Types/ActionTypes.js";
+
+export const addToCart = (productId, product) => async (dispatch) => {
+  console.log("productId:", productId, "product:", product);
+  return dispatch({
+    type: type.ADD_TO_CART,
+    payload: {
+      id: productId,
+      product: product,
+    },
+  });
+};
+
+export const removeFromCart = (productId) => async (dispatch) => {
+  console.log("id", productId);
+  return dispatch({
+    type: type.REMOVE_FROM_CART,
+    payload: {
+      id: productId,
+    },
+  });
+};
+
+export const adjustQuantity = (productId, value) => async (dispatch) => {
+  return dispatch({
+    type: type.ADJUST_QUANTITY,
+    payload: {
+      id: productId,
+      qty: value,
+    },
+  });
+};
+
+export const loadCurrentProduct = (product) => async (dispatch) => {
+  return dispatch({
+    type: type.LOAD_CURRENT_ITEM,
+    payload: product,
+  });
+};
